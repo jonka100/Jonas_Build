8d057cbef0ea31b79f6ee1b00252626e8e711986
Jonas Karlsson <jonas.karlsson@valtech.se>
2015-02-09 14:36:22 +0100

    Added unit tests

---
+++ UnitTest/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace UnitTest
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void test()
+        {
+            Assert.That(1, Is.EqualTo(1));
+        }
+    }
+}
